4fe906c86b1adba5d929768f3aaf22c28c17d22b
Jackson-Cong <1315187655@qq.com>
2019-10-26 14:56:42 +0800

    function

---
+++ try0.py
@@ -0,0 +1,12 @@
+#-*-coding:UTF-8 -*-
+
+# author by: Jackson
+
+def pnt_cut_line(sn):
+    'Desc: Print a cutting line for me'
+    print('\n--------------- No (%d) Cutting Line ----------------'% (sn))
+    return
+
+# call function
+for idx in range(5):
+    pnt_cut_line(idx)
